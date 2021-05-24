2f57bc01a09ce71f17d830fa67747e6bbb1aa543
Leon Kim <serialexperimentsleon@gmail.com>
2021-05-24 11:55:17 -0400

    add cart force and imped controller from rachel holl fork

---
+++ franka_ros_controllers/src/cartesian_force_controller.cpp
@@ -0,0 +1,163 @@
+  
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+
+// Taken from rachelhollday's fork of justagist/franka_ros_interface
+// https://github.com/rachelholladay/franka_ros_interface/blob/feature/newControllers/franka_ros_controllers/src/cartesian_force_controller.cpp
+
+#include <franka_ros_controllers/cartesian_force_controller.h>
+
+#include <cmath>
+#include <memory>
+
+#include <controller_interface/controller_base.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+#include <franka/robot_state.h>
+
+namespace franka_ros_controllers {
+
+bool CartesianForceController::init(hardware_interface::RobotHW* robot_hw,
+                                  ros::NodeHandle& node_handle) {
+  std::vector<std::string> joint_names;
+  std::string arm_id;
+
+  force_params_ = node_handle.subscribe(
+    "/wrench_target", 20, &CartesianForceController::forceParamCallback, this,
+    ros::TransportHints().reliable().tcpNoDelay());
+
+  ROS_WARN(
+      "CartesianForceController: Make sure your robot's endeffector is in contact "
+      "with a horizontal surface before starting the controller!");
+  if (!node_handle.getParam("arm_id", arm_id)) {
+    ROS_ERROR("CartesianForceController: Could not read parameter arm_id");
+    return false;
+  }
+  if (!node_handle.getParam("joint_names", joint_names) || joint_names.size() != 7) {
+    ROS_ERROR(
+        "CartesianForceController: Invalid or no joint_names parameters provided, aborting "
+        "controller init!");
+    return false;
+  }
+
+  auto* model_interface = robot_hw->get<franka_hw::FrankaModelInterface>();
+  if (model_interface == nullptr) {
+    ROS_ERROR_STREAM("CartesianForceController: Error getting model interface from hardware");
+    return false;
+  }
+  try {
+    model_handle_ = std::make_unique<franka_hw::FrankaModelHandle>(
+        model_interface->getHandle(arm_id + "_model"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "CartesianForceController: Exception getting model handle from interface: " << ex.what());
+    return false;
+  }
+
+  auto* state_interface = robot_hw->get<franka_hw::FrankaStateInterface>();
+  if (state_interface == nullptr) {
+    ROS_ERROR_STREAM("CartesianForceController: Error getting state interface from hardware");
+    return false;
+  }
+  try {
+    state_handle_ = std::make_unique<franka_hw::FrankaStateHandle>(
+        state_interface->getHandle(arm_id + "_robot"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "CartesianForceController: Exception getting state handle from interface: " << ex.what());
+    return false;
+  }
+
+  auto* effort_joint_interface = robot_hw->get<hardware_interface::EffortJointInterface>();
+  if (effort_joint_interface == nullptr) {
+    ROS_ERROR_STREAM("CartesianForceController: Error getting effort joint interface from hardware");
+    return false;
+  }
+  for (size_t i = 0; i < 7; ++i) {
+    try {
+      joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
+    } catch (const hardware_interface::HardwareInterfaceException& ex) {
+      ROS_ERROR_STREAM("CartesianForceController: Exception getting joint handles: " << ex.what());
+      return false;
+    }
+  }
+
+  // Initialize
+  //target_mass_ = 0.0;
+  target_mass_.setZero();
+  desired_mass_.setZero();
+  return true;
+}
+
+void CartesianForceController::starting(const ros::Time& /*time*/) {
+  franka::RobotState robot_state = state_handle_->getRobotState();
+  std::array<double, 7> gravity_array = model_handle_->getGravity();
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_measured(robot_state.tau_J.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> gravity(gravity_array.data());
+  // Bias correction for the current external torque
+  tau_ext_initial_ = tau_measured - gravity;
+  tau_error_.setZero();
+}
+
+void CartesianForceController::update(const ros::Time& /*time*/, const ros::Duration& period) {
+  franka::RobotState robot_state = state_handle_->getRobotState();
+  std::array<double, 42> jacobian_array =
+      model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
+  std::array<double, 7> gravity_array = model_handle_->getGravity();
+  Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_measured(robot_state.tau_J.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d(  // NOLINT (readability-identifier-naming)
+      robot_state.tau_J_d.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> gravity(gravity_array.data());
+
+  Eigen::VectorXd tau_d(7), desired_force_torque(6), tau_cmd(7), tau_ext(7);
+  desired_force_torque.setZero();
+  for (size_t i = 0; i < 7; ++i) {
+      desired_force_torque(i) = desired_mass_(i); // * -9.81;
+  }
+  //desired_force_torque(2) = desired_mass_(2) * -9.81;
+  tau_ext = tau_measured - gravity - tau_ext_initial_;
+  tau_d << jacobian.transpose() * desired_force_torque;
+  tau_error_ = tau_error_ + period.toSec() * (tau_d - tau_ext);
+  // FF + PI control (PI gains are initially all 0)
+  tau_cmd = tau_d + k_p_ * (tau_d - tau_ext) + k_i_ * tau_error_;
+  tau_cmd << saturateTorqueRate(tau_cmd, tau_J_d);
+
+  for (size_t i = 0; i < 7; ++i) {
+    joint_handles_[i].setCommand(tau_cmd(i));
+  }
+
+  // Update signals changed online through dynamic reconfigure
+  desired_mass_ = filter_gain_ * target_mass_ + (1 - filter_gain_) * desired_mass_;
+  k_p_ = filter_gain_ * target_k_p_ + (1 - filter_gain_) * k_p_;
+  k_i_ = filter_gain_ * target_k_i_ + (1 - filter_gain_) * k_i_;
+}
+
+void CartesianForceController::forceParamCallback(
+     const geometry_msgs::Wrench& msg) {
+
+  target_mass_(0) = msg.force.x;
+  target_mass_(1) = msg.force.y;
+  target_mass_(2) = msg.force.z;
+  target_mass_(3) = msg.torque.x;
+  target_mass_(4) = msg.torque.y;
+  target_mass_(5) = msg.torque.z;
+}
+
+
+Eigen::Matrix<double, 7, 1> CartesianForceController::saturateTorqueRate(
+    const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+    const Eigen::Matrix<double, 7, 1>& tau_J_d) {  // NOLINT (readability-identifier-naming)
+  Eigen::Matrix<double, 7, 1> tau_d_saturated{};
+  for (size_t i = 0; i < 7; i++) {
+    double difference = tau_d_calculated[i] - tau_J_d[i];
+    tau_d_saturated[i] = tau_J_d[i] + std::max(std::min(difference, kDeltaTauMax), -kDeltaTauMax);
+  }
+  return tau_d_saturated;
+}
+
+}  // namespace franka_ros_controllers
+
+PLUGINLIB_EXPORT_CLASS(franka_ros_controllers::CartesianForceController,
+                       controller_interface::ControllerBase)
